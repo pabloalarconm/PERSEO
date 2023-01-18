d1763a0f56586e22bad76b639d5a5d2de2c26b0e
pabloalarconm <pabloalarconmoreno@gmail.com>
2023-01-18 07:34:13 +0100

    implementation new

--- test_hefesto/test_hefesto.py
+++ test_hefesto/test_hefesto.py
@@ -2,13 +2,13 @@ from Hefesto.main import Hefesto
 import yaml
 
 # Import YAML configuration file with all parameters:
-with open("DCDEconfig.yaml") as file:
+with open("CDEconfig.yaml") as file:
     configuration = yaml.load(file, Loader=yaml.FullLoader)
 
-test = Hefesto(datainput = "DCDE_input.csv")
+test = Hefesto(datainput = "OFFICIAL_DATA_INPUT.csv")
 transform = test.transform_shape(configuration=configuration)
 # label = test.get_label("outputURI")
 # url_from_label= test.get_uri("outputURI_label","ncit")
 # repl= test.replacement("outputURI_label", "Date","DateXXX", duplicate=False)
 
-transform.to_csv ("result_DCDE.csv", index = False, header=True)
+transform.to_csv ("result_CDE2.csv", index = False, header=True)
