cf11b0c3d7f0e92d33e4f3fd50620a73d4ca1336
Pablo Alarcón <pabloalarconmoreno@gmail.com>
2025-01-02 14:12:01 +0100

    cleanance

--- OMOP2CDE/Postgres2RDF/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo python3 -m rdfizer -c config.ini
-sudo python3 nt2ttl.py ${PWD}/data/graph
-sudo chmod -R 777 .
--- cde_implementation/test_quads.py
+++
@@ -1,6 +0,0 @@
-
-from rdflib import Graph, Dataset, ConjunctiveGraph
-
-g = Dataset()
-g.parse("/home/pabloalarconm/Desktop/EJP/PERSEO/cde_implementation/data/triples/unifiedCDE_one.nt", format="nquads")
-g.serialize(destination="result.ttl", format="trig")
